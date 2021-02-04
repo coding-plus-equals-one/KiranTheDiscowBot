aeb52813e6e1350d4b8ee1137a1ed2d2f75ad641
Alexander Zhang <zhangalexy@gmail.com>
2021-02-03 16:56:34 -0800

    Add task list

--- kiran.py
+++ kiran.py
@@ -9,6 +9,8 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+tasks = []
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -16,5 +18,9 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         await message.channel.send(f'Hello, {message.author.display_name}!')
+    elif message.content.startswith('!addTask '):
+        tasks.append(message.content.split(' ', 1)[1])
+    elif message.content.startswith('!tasks'):
+        await message.channel.send('\n'.join(tasks))
 
 client.run(os.environ['KIRAN_TOKEN'])
