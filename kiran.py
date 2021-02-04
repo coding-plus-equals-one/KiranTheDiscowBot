ea391a7fcef40c373477d0776ceafa60dec07d0b
Alexander Zhang <zhangalexy@gmail.com>
2021-02-03 16:25:34 -0800

    Create kiran.py

---
+++ kiran.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ['KIRAN_TOKEN'])
