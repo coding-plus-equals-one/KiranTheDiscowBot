5f9e17ff6aabb91d86916e692bdf489d700125b5
Alexander Zhang <zhangalexy@protonmail.com>
2021-02-13 22:22:24 -0800

    Change mention to display name, add no tasks message

--- kiran.py
+++ kiran.py
@@ -20,7 +20,7 @@ async def on_message(message):
 
     try:
         if message.content == '!hello':
-            await message.channel.send(f'Hello, {message.author.mention}!')
+            await message.channel.send(f'Hello, {message.author.display_name}!')
         elif message.content.startswith('!task '):
             subcommand = message.clean_content.split(' ', 1)[1]
             if message.guild not in tasks:
@@ -32,11 +32,17 @@ async def on_message(message):
                 if len(message.mentions) > 0:
                     await message.channel.send(' '.join([user.mention for user in message.mentions]) + ' You have a new task!')
             elif subcommand == 'list':
-                await message.channel.send('\n'.join([f'{i + 1}. {task}' for i, task in zip(range(len(tasks[message.guild])), tasks[message.guild])]))
+                if len(tasks[message.guild]) == 0:
+                    await message.channel.send('There are no tasks. Yay!')
+                else:
+                    await message.channel.send('\n'.join([f'{i + 1}. {task}' for i, task in zip(range(len(tasks[message.guild])), tasks[message.guild])]))
             elif subcommand.startswith('remove '):
                 task_index = int(subcommand.split(' ', 1)[1]) - 1
                 task = tasks[message.guild].pop(task_index)
                 await message.channel.send('Deleted task ' + task)
+            elif subcommand == 'clear':
+                tasks[message.guild].clear()
+                await message.channel.send('Cleared tasks')
     except:
         pass
 
