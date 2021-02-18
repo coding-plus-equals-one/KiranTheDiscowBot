4a0610dc2745ebb2d10c359f6534f9dc852c62b6
Alexander Zhang <zhangalexy@protonmail.com>
2021-02-17 16:32:38 -0800

    Improve argument parsing

--- kiran.py
+++ kiran.py
@@ -2,6 +2,8 @@
 
 import discord
 import os
+import shlex
+import collections
 
 client = discord.Client()
 
@@ -19,30 +21,33 @@ async def on_message(message):
     # print(message.content)
 
     try:
-        if message.content == '!hello':
-            await message.channel.send(f'Hello, {message.author.display_name}!')
-        elif message.content.startswith('!task '):
-            subcommand = message.clean_content.split(' ', 1)[1]
-            if message.guild not in tasks:
-                tasks[message.guild] = []
-            if subcommand.startswith('add '):
-                task_name = subcommand.split(' ', 1)[1]
-                tasks[message.guild].append(task_name)
-                await message.channel.send('Added task ' + task_name)
-                if len(message.mentions) > 0:
-                    await message.channel.send(' '.join([user.mention for user in message.mentions]) + ' You have a new task!')
-            elif subcommand == 'list':
-                if len(tasks[message.guild]) == 0:
-                    await message.channel.send('There are no tasks. Yay!')
-                else:
-                    await message.channel.send('\n'.join([f'{i + 1}. {task}' for i, task in zip(range(len(tasks[message.guild])), tasks[message.guild])]))
-            elif subcommand.startswith('remove '):
-                task_index = int(subcommand.split(' ', 1)[1]) - 1
-                task = tasks[message.guild].pop(task_index)
-                await message.channel.send('Deleted task ' + task)
-            elif subcommand == 'clear':
-                tasks[message.guild].clear()
-                await message.channel.send('Cleared tasks')
+        if message.content.startswith('!'):
+            args = collections.deque(shlex.split(message.clean_content[1:]))
+            command = args.popleft()
+            if command == 'hello':
+                await message.channel.send(f'Hello, {message.author.display_name}!')
+            elif command == 'task':
+                if message.guild not in tasks:
+                    tasks[message.guild] = []
+                subcommand = args.popleft()
+                if subcommand == 'add':
+                    task_name = args.popleft()
+                    tasks[message.guild].append(task_name)
+                    await message.channel.send('Added task ' + task_name)
+                    if len(message.mentions) > 0:
+                        await message.channel.send(' '.join([user.mention for user in message.mentions]) + ' You have a new task!')
+                elif subcommand == 'list':
+                    if len(tasks[message.guild]) == 0:
+                        await message.channel.send('There are no tasks. Yay!')
+                    else:
+                        await message.channel.send('\n'.join([f'{i + 1}. {task}' for i, task in zip(range(len(tasks[message.guild])), tasks[message.guild])]))
+                elif subcommand == 'remove':
+                    task_index = int(args.popleft()) - 1
+                    task = tasks[message.guild].pop(task_index)
+                    await message.channel.send('Deleted task ' + task)
+                elif subcommand == 'clear':
+                    tasks[message.guild].clear()
+                    await message.channel.send('Cleared tasks')
     except:
         pass
 
