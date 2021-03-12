593f3b871f5883d64f7ef55a5ad33a33d4f03d4d
Alexander Zhang <zhangalexy@protonmail.com>
2021-03-11 16:43:41 -0800

    Add brief help text

--- kiran.py
+++ kiran.py
@@ -15,30 +15,30 @@ async def on_ready():
 
 tasks = {}
 
-@bot.command()
+@bot.command(brief='Say hello')
 async def hello(ctx):
     await ctx.send(f'Hello, {ctx.author.display_name}!')
 
-@bot.group()
+@bot.group(brief='Manage tasks')
 async def task(ctx):
     if ctx.guild not in tasks:
         tasks[ctx.guild] = []
 
-@task.command()
+@task.command(brief='Add new task')
 async def add(ctx, *, arg: commands.clean_content):
     tasks[ctx.guild].append(arg)
     await ctx.send('Added task ' + arg)
     if len(ctx.message.mentions) > 0:
         await ctx.send(' '.join([user.mention for user in ctx.message.mentions]) + ' You have a new task!')
 
-@task.command()
+@task.command(brief='List tasks')
 async def list(ctx):
     if len(tasks[ctx.guild]) == 0:
         await ctx.send('There are no tasks. Yay!')
     else:
         await ctx.send('\n'.join([f'{i + 1}. {task}' for i, task in zip(range(len(tasks[ctx.guild])), tasks[ctx.guild])]))
 
-@task.command()
+@task.command(brief='Remove task specified by its number')
 async def remove(ctx, task_index: int):
     task_index -= 1
     try:
@@ -47,17 +47,17 @@ async def remove(ctx, task_index: int):
     except IndexError:
         await ctx.send('No such task')
 
-@task.command()
+@task.command(brief='Remove all tasks')
 async def clear(ctx):
     tasks[ctx.guild].clear()
     await ctx.send('Cleared tasks')
 
-@bot.command()
+@bot.command(brief='Echo the arguments')
 async def say(ctx, *, arg):
     await ctx.send(arg)
 
-@bot.command()
+@bot.command(brief='Send dancing cow GIF')
 async def dance(ctx):
     await ctx.send(file=discord.File('dance.gif'))
-
+brief='Send dancing cow GIF'
 bot.run(os.environ['KIRAN_TOKEN'])
