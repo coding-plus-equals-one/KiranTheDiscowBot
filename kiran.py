6575d88c7e76b25bc16cd8005cc6ada0f104468f
Alexander Zhang <zhangalexy@protonmail.com>
2021-03-17 21:07:15 -0700

    Remove leftover tempfile line

--- kiran.py
+++ kiran.py
@@ -98,7 +98,6 @@ async def speak(ctx, *, message: commands.clean_content):
         if ctx.voice_client.is_playing():
             ctx.voice_client.stop()
         await ctx.voice_client.move_to(ctx.author.voice.channel)
-    msg_fp = tempfile.TemporaryFile()
     tts = gTTS(message, lang='en')
     tts.save('message.mp3')
     source = discord.FFmpegPCMAudio('message.mp3')
